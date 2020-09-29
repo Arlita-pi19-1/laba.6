968bc103b625c35de6c1cdb3437a175ddb1d4b5b
Arlita-pi19-1 <protopopova.aelita@gmail.com>
2020-09-29 18:02:26 +0900

    6.4

---
+++ src/Buf_4.java
@@ -0,0 +1,32 @@
+import java.io.*;
+public class Buf_4 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try {
+// Создание потоков для чтения и записи с нужной кодировкой
+            br = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream("E:\\MyFile1.txt"), "cp1251"));
+            bw = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream("E:\\MyFile2.txt"), "cp1251"));
+// Переписывание информации из одного файла в другой
+            int lineCount = 0;
+            String s;
+            while ((s = br.readLine()) != null) {
+                lineCount++;
+                System.out.println(lineCount + ": " + s);
+                bw.write(lineCount + ": " + s);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка !!!!!!!!");
+        } finally {
+            br.close();
+            bw.flush();
+            bw.close();
+        }
+    }
+}
